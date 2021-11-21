f06ab87e0fe0cdca040778d67cb8acb576f3dd08
a23246542 <a23246542@gmail.com>
2021-11-21 17:24:06 +0800

    chore: install react-router-dom v6 & antd,emotion

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@primary-color': '#0052cc',
+              '@font-size-base:': '16px',
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
