c0057da3f6298bd52be566dcefa4bcca9a22c275
a23246542 <a23246542@gmail.com>
2021-12-06 15:25:33 +0800

    feat: connect msw service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadDevTools} from 'jira-dev-tool';
 
 import 'antd/dist/antd.less';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+loadDevTools(()=>{
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
