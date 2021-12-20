291143a6e3bd130d4ada4e758c2b90578bfbc924
a23246542 <a23246542@gmail.com>
2021-12-21 03:01:20 +0800

    feat: react suspense to code split

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthenticatedApp, UnauthenticatedApp } from 'routes/index';
-
 import { useAuth } from 'hooks/useAuth';
-
+import { FullPageLoading } from 'components/UI';
 import './App.css';
+
+const AuthenticatedApp = React.lazy(
+  () => import('routes/AuthenticatedApp'),
+);
+const UnauthenticatedApp = React.lazy(
+  () => import('routes/UnauthenticatedApp'),
+);
 
 function App() {
   const { user } = useAuth();
   return (
     <Router>
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <React.Suspense fallback={<FullPageLoading />}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </React.Suspense>
     </Router>
   );
 }
